9f8183c24ac596fb9d2185aadee41ff3a755fae7
Angelo <angepili@icloud.com>
2022-11-24 23:10:35 +0100

    feat: improve pagination

--- pager.class.php
+++ pager.class.php
@@ -10,7 +10,7 @@ class Pager {
         $this->offset = 0;
     }
 
-    public function limit($limit = 10){
+    public function limit($limit = 80){
         $this->limit = $limit ? $limit : 10;
     }
 
@@ -28,12 +28,34 @@ class Pager {
     }
 
     public function paginator(){
-        $pages = floor(count($this->data) / $this->limit) -1 ;
+    
+        $currentUrl = $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+        $pages = (int)floor(count($this->data) / $this->limit) -1 ;
+    
         for($i = 0; $i <= $pages; $i++){
-            $this->paginator[$i]['anchor'] = $i + 1;
-            $this->paginator[$i]['link']   = '?p='.$i; 
+            if( $i === 0 ) {
+                $this->paginator[$i]['anchor'] = 'Inizio';
+            } elseif( $pages === $i  ) {
+                $this->paginator[$i]['anchor'] = 'Fine';
+            } else {
+                $this->paginator[$i]['anchor'] = $i;
+            }
+
+            $this->paginator[$i]['link']   = $this->url_pagination( $currentUrl, "p", $i ); 
         }
         return $this->paginator;
     }
 
+    public function url_pagination($url, $parameterName, $parameterValue ) {
+        $url = parse_url($url);
+        parse_str($url["query"],$parameters);
+        unset($parameters[$parameterName]);
+        $parameters[$parameterName]=$parameterValue;
+        return sprintf("%s://%s%s?%s", 
+            $url["scheme"],
+            $url["host"],
+            $url["path"],
+            http_build_query($parameters));
+    }
 }
